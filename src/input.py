b0251f04d7cd4f5757e046e504b4ef2ec623f384
Reuynil <374386252@qq.com>
2015-02-03 23:10:02 +0800

    添加了两个功能：可以读取所有的packet的头；可以得到packe的个数。

--- src/input.py
+++ src/input.py
@@ -4,66 +4,96 @@ import os
 import sys
 import types
 import collections
+import struct
+
 
 class PcapHeader:
     '''
     This is the header of Pcap file.
     '''
-    __field=collections.OrderedDict()
-    def __init__(self,pcapFile):
-        if(type(pcapFile)==types.FileType):
-            if(not pcapFile.closed):
-                pcapFile.seek(0,0)
+    __field = collections.OrderedDict()
+
+    def __init__(self, pcapFile):
+        if (type(pcapFile) == types.FileType):
+            if (not pcapFile.closed):
+                pcapFile.seek(0, 0)
                 self.__field['Magic'] = pcapFile.read(4).encode('hex')
-                self.__field['Major']=pcapFile.read(2).encode('hex')
-                self.__field['Minor']=pcapFile.read(2).encode('hex')
-                self.__field['ThisZone']=pcapFile.read(4).encode('hex')
-                self.__field['SigFigs']=pcapFile.read(4).encode('hex')
-                self.__field['SanpLen']=pcapFile.read(4).encode('hex')
-                self.__field['LinkType']=pcapFile.read(4).encode('hex')
+                self.__field['Major'] = pcapFile.read(2).encode('hex')
+                self.__field['Minor'] = pcapFile.read(2).encode('hex')
+                self.__field['ThisZone'] = pcapFile.read(4).encode('hex')
+                self.__field['SigFigs'] = pcapFile.read(4).encode('hex')
+                self.__field['SanpLen'] = pcapFile.read(4).encode('hex')
+                self.__field['LinkType'] = pcapFile.read(4).encode('hex')
         else:
             print "Can not open the pcapFile."
-        
+
     def __str__(self):
         str = ''
         for key in self.__field:
-            str = str+key.ljust(16)+self.__field[key]+'\n'
+            str = str + key.ljust(16) + self.__field[key] + '\n'
         return str
-    
+
+
 class PacketHeader:
     '''
     This is the header of Packet
     '''
-    __field=collections.OrderedDict()
-    def __init__(self,pcapFile,whence):
-        if(type(pcapFile)==types.FileType):
-            if(not pcapFile.closed):
-                pcapFile.seek(whence,0)
-                self.__field['TimestampHigh']=pcapFile.read(4).encode('hex')
-                self.__field['TimestampLow']=pcapFile.read(4).encode('hex')
-                self.__field['Caplen']=pcapFile.read(4).encode('hex')
-                self.__field['Length']=pcapFile.read(4).encode('hex')
+    __field = collections.OrderedDict()
+
+    def __init__(self, pcapFile, whence):
+        self.__fieldItem = ['TimestampSec', 'TimestampMSe', 'Caplen', 'Length']
+        if (type(pcapFile) == types.FileType):
+            if (not pcapFile.closed):
+                pcapFile.seek(whence, 0)
+                fieldData = struct.unpack('4I', pcapFile.read(16))
+                index = 0
+                for key in self.__fieldItem:
+                    self.__field[key] = fieldData[index]
+                    index = index + 1
         else:
             print "Can not open the pcapFile."
 
     def getPacketLength(self):
         return self.__field['Caplen']
 
+    def __str__(self):
+        tempStr = ''
+        for key in self.__field:
+            tempStr = tempStr + key.ljust(16) + str(self.__field[key]) + '\n'
+        return tempStr
+
+
 class PcapFile:
-    def __init__(self,fileName):
-        self.__pcapLength=os.path.getsize(fileName)
-        self.__pcapFile=open(fileName,'rb')
-        self.__pcapHeader=PcapHeader(self.__pcapFile)
-    
+    def __init__(self, fileName):
+        self.__pcapLength = int(os.path.getsize(fileName))
+        self.__pcapFile = open(fileName, 'rb')
+        self.__pcapHeader = PcapHeader(self.__pcapFile)
+        self.getPacketHeader()
+
     def __len__(self):
         return self.__pcapLength
-    
-    def getPcapHeader(self): 
+
+    def getPcapHeader(self):
         return self.__pcapHeader
-    
+
     def __del__(self):
         self.__pcapFile.close()
-        
-        
-        
+
+    def getPacketHeader(self):
+        self.__packetHeader = []
+        whence = 24
+        while whence < self.__pcapLength:
+            x = PacketHeader(self.__pcapFile, whence)
+            whence = whence + x.getPacketLength() + 16
+            self.__packetHeader.append(x)
+
+    def getPacketHeaderNum(self):
+        return len(self.__packetHeader)
+
+    def getPacketData(self):
+        pass
+
+    def followTCPstream(self, ip):
+        pass
+
         
--- src/main.py
+++ src/main.py
@@ -3,15 +3,17 @@
 from input import *
 
 if __name__ == '__main__':
-    file_name="weather_channel_android_app.pcap"
-    
-    src_path=sys.path[0]
-    program_path=os.path.abspath(os.path.join(src_path,os.pardir))
-    input_path=os.path.join(program_path,'input')
-    output_path=os.path.join(program_path,'output')
-    
-    input_file=os.path.join(input_path,file_name)    
+    file_name = "weather_channel_android_app.pcap"
 
-    pcap_file=PcapFile(input_file)
-    test=pcap_file.getPcapHeader()
+    src_path = sys.path[0]
+    program_path = os.path.abspath(os.path.join(src_path, os.pardir))
+    input_path = os.path.join(program_path, 'input')
+    output_path = os.path.join(program_path, 'output')
+
+    input_file = os.path.join(input_path, file_name)
+
+    pcap_file = PcapFile(input_file)
+    test = pcap_file.getPcapHeader()
     print test
+    print len(pcap_file)
+    print pcap_file.getPacketHeaderNum()
