4f24a8c5544d8c0ff41aecd1a25c93a58cb9ae89
Reuynil <374386252@qq.com>
2015-03-16 16:03:26 +0800

    tokenize the messages.

--- src/tokenize.py
+++ src/tokenize.py
@@ -1,64 +1,67 @@
-# -*- encoding=cp936 -*-
+# -*- encoding=utf-8 -*-
 
 __author__ = 'Reuynil'
 
 from utility import *
 
-class Text:
-    def __init__(self, string_data):
-        self.data = string_data
-        print(type(self.data), repr(self.data))
+
+class token:
+    def __init__(self, t_data, t_type='B', t_variable=True):
+        self.data = t_data
+        self.type = t_type
+        self.variable = t_variable
 
     def __str__(self):
-        return self.data.decode('utf8').encode('gbk')
+        return "type:\t" + self.type + "\ndata:\t" + repr(self.data)
+
 
 def tokenization(data):
     '''
-    ¸ù¾İutf-8±àÂë¹æÔòÅĞ¶ÏÊı¾İÊÇ·ñÎª×Ö·û£¬ÊÇµÄ»°½«¶ÔÓ¦×Ö¶Î¼ÇÎªT£¬²»ÊÇ¼ÇÎªB¡£
-    :param data:×Ö·û´®£¬²»¾­¹ıÈÎºÎ±àÂë¡£
-    :return:Ò»¸öÁĞ±í£¬ÔªËØÊÇ±ê¼Ç¡£
+    æ ¹æ®utf-8ç¼–ç è§„åˆ™åˆ¤æ–­æ•°æ®æ˜¯å¦ä¸ºå­—ç¬¦ï¼Œæ˜¯çš„è¯å°†å¯¹åº”å­—æ®µè®°ä¸ºTï¼Œä¸æ˜¯è®°ä¸ºBã€‚
+    :param data:å­—ç¬¦ä¸²ï¼Œä¸ç»è¿‡ä»»ä½•ç¼–ç ã€‚
+    :return:ä¸€ä¸ªåˆ—è¡¨ï¼Œå…ƒç´ æ˜¯æ ‡è®°ã€‚
     '''
     token_patten = []
     data_length = len(data)
     k = 0
     while k < data_length:
-        first_byte = data[k]
+        first_byte = data[k:k + 1]
         first_byte_int = byteToInt(first_byte)
         if not testBit(first_byte_int, 7):
             try:
                 decode_data = first_byte.decode('ascii')
             except Exception as e:
-                token_patten.append('B')
+                token_patten.append(token(first_byte, 'B'))
             else:
-                token_patten.append(Text(first_byte))
+                token_patten.append(token(first_byte, 'T'))
             finally:
                 k += 1
         elif not testBit(first_byte_int, 6):
-            token_patten.append('B')
+            token_patten.append(token(first_byte, 'B'))
             k += 1
         else:
             i = 5
             while i > 0 and testBit(first_byte_int, i):
                 i -= 1
             if i < 2:
-                token_patten.append('B')
+                token_patten.append(token(first_byte, 'B'))
                 k += 1
             else:
                 remain_byte_length = 6 - i
                 try:
                     remain_byte = data[k + 1:k + remain_byte_length + 1]
                 except Exception as e:
-                    token_patten.append('B')
+                    token_patten.append(token(first_byte, 'B'))
                     k += 1
                 else:
                     full_byte = first_byte + remain_byte
                     try:
                         decode_data = full_byte.decode('utf8')
                     except Exception as e:
-                        token_patten.append('B')
+                        token_patten.append(token(first_byte, 'B'))
                         k += 1
                     else:
-                        token_patten.append(Text(full_byte))
+                        token_patten.append(token(full_byte, 'T'))
                         k = k + remain_byte_length + 1
     return token_patten
 
@@ -66,8 +69,7 @@ def tokenization(data):
 if __name__ == "__main__":
     f = open('test_text.txt', 'rb')
     data = f.read()
-    print(data.encode('hex'))
-    token = tokenization(data)
+    token_patten_list = tokenization(data)
     print('=' * 60)
-    for item in token:
+    for item in token_patten_list:
         print(item)
