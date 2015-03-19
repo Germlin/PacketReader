d461c36559192dbccf98748cd1f7075db87a2582
Reuynil <374386252@qq.com>
2015-03-19 11:41:08 +0800

    class message contains of  data, direction and token priority.

---
+++ src/message.py
@@ -0,0 +1,13 @@
+# -*- encoding=utf-8 -*-
+
+__author__ = 'Reuynil'
+
+
+class message:
+    def __init__(self, token_priority, message_data, t_destination, t_source):
+        assert isinstance(token_priority, list)
+        self.priority_list = token_priority
+        self.data = message_data
+        self.destination = t_destination
+        self.source = t_source
+
