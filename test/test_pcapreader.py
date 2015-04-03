0b88ccbeee605a9dc76dbdfde1172729de521d9c
Reuynil <374386252@qq.com>
2015-04-03 20:41:13 +0800

    更正pcapreader模块中的警告

---
+++ test/test_pcapreader.py
@@ -0,0 +1,23 @@
+# -*- encoding=utf-8 -*-
+
+__author__ = 'linyue'
+
+import ProtocolReverse
+import sys
+import os
+
+
+if __name__ == "__main__":
+    file_name = "weather_channel_android_app.pcap"
+    test_path = sys.path[0]
+    program_path = os.path.abspath(os.path.join(test_path, os.pardir))
+    input_path = os.path.join(program_path, 'input')
+    output_path = os.path.join(program_path, 'output')
+    input_file = os.path.join(input_path, file_name)
+
+    pcap_file=ProtocolReverse.pcapreader.PcapFile(input_file)
+    pcap_file_header=pcap_file.get_pcap_header()
+    print(pcap_file_header)
+
+    packet_list=pcap_file.get_packet()
+    print(len(packet_list))
