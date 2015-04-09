8d68dd2008984446fe2c5d376009a5c39a2df237
Reuynil <374386252@qq.com>
2015-04-09 23:41:06 +0800

    增加了Smith-Waterman算法

--- ProtocolReverse/alignment.py
+++ ProtocolReverse/alignment.py
@@ -16,13 +16,20 @@ def needleman_wunsch_alignment(seq_1, seq_2):
     seq_2_len = len(seq_2)
     mx = zeros((seq_1_len + 1, seq_2_len + 1))
 
+    # 初始化得分矩阵
+    for i in range(0, seq_1_len + 1):
+        mx[i][0] = 0 - i
+
+    for i in range(0, seq_2_len + 1):
+        mx[0][i] = 0 - i
+
     # 计算匹配矩阵
     for i in range(1, seq_1_len + 1):
         for j in range(1, seq_2_len + 1):
             if seq_1[i - 1] == seq_2[j - 1]:
                 mx[i][j] = mx[i - 1][j - 1] + 1
             else:
-                mx[i][j] = max(mx[i - 1][j - 1], mx[i - 1][j], mx[i][j - 1])
+                mx[i][j] = max([mx[i - 1][j - 1], mx[i - 1][j], mx[i][j - 1]])
 
     lcs = 0
     new_seq = list()
@@ -56,9 +63,56 @@ def needleman_wunsch_alignment(seq_1, seq_2):
     return lcs, new_seq
 
 
-def smith_waterman_alignment(seq_1,seq_2):
-    pass
+def smith_waterman_alignment(seq_1, seq_2):
+    """
+    Smith-Waterman算法，用于计算两个序列之间的距离。
+    :param seq_1:
+    :param seq_2:
+    :return:
+    """
+    seq_1_len = len(seq_1)
+    seq_2_len = len(seq_2)
+    mx = zeros((seq_1_len + 1, seq_2_len + 1))
 
+    # 计算匹配矩阵
+    max_score = 0
+    max_i = 0
+    max_j = 0
+    for i in range(1, seq_1_len + 1):
+        for j in range(1, seq_2_len + 1):
+            if seq_1[i - 1] == seq_2[j - 1]:
+                mx[i][j] = mx[i - 1][j - 1] + 2
+            else:
+                mx[i][j] = max([mx[i - 1][j - 1] - 1, mx[i - 1][j] - 2, mx[i][j - 1] - 2, 0])
 
-def upmga():
-    pass
+            if mx[i][j] > max_score:
+                max_score = mx[i][j]
+                max_i = i
+                max_j = j
+    lcs = 0
+
+    # 开始回溯
+    i = max_i
+    j = max_j
+    while 0 != mx[i][j]:
+        if i == 0:
+            j -= 1
+        elif j == 0:
+            i -= 1
+        elif seq_1[i - 1] == seq_2[j - 1]:
+            lcs += 1
+            i -= 1
+            j -= 1
+        else:
+            t = (mx[i - 1][j - 1], mx[i - 1][j], mx[i][j - 1])
+            max_value = max(t)
+            index = t.index(max_value)
+            if index == 0:
+                i -= 1
+                j -= 1
+            elif index == 1:
+                i -= 1
+            elif index == 2:
+                j -= 1
+
+    return lcs
--- test/test_alignment.py
+++ test/test_alignment.py
@@ -4,13 +4,46 @@ __author__ = 'linyue'
 
 
 import ProtocolReverse
+import timeit
+import random
 
+
+def timer_nw():
+    global seq1,seq2
+    lcs,seq=ProtocolReverse.alignment.needleman_wunsch_alignment(seq1,seq2)
+    return lcs,seq
+
+def timer_sw():
+    global seq1,seq2
+    lcs=ProtocolReverse.alignment.smith_waterman_alignment(seq1,seq2)
+    return lcs
 
 if __name__=="__main__":
-    seq1=["T","A","C","G","C","G","A"]
-    seq2=["A","G","C","G","C","T"]
 
-    lcs,seq=ProtocolReverse.alignment.needleman_wunsch_alignment(seq1,seq2)
-    print(lcs)
-    for i in seq:
-        print(i)
+    time = 1
+    tsw = tnw = 0
+    while time <= 10000:
+        seq1=list()
+        for i in range(random.randint(10,1000)):
+            seq1.append(random.choice(["A","C","G","T"]))
+        #print(seq1)
+        seq2=list()
+        for j in range(random.randint(10,1000)):
+            seq2.append(random.choice(["A","C","G","T"]))
+        #print(seq2)
+
+        tsw += timeit.timeit('timer_sw',setup="from __main__ import timer_sw;")
+        tnw += timeit.timeit('timer_nw',setup="from __main__ import timer_nw;")
+        print(time,tsw,tnw)
+
+        time += 1
+
+    print(tsw,tnw)
+    # lcs, seq = timer_nw()
+    # print(seq1)
+    # print(seq2)
+    # print(lcs)
+    # for i in seq:
+    #     print(i)
+    #
+    # print(timer_sw())
