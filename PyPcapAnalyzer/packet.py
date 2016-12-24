389d35aecefc935800102281364c70f48516b337
Yue <linyue92@outlook.com>
2016-12-24 23:30:37 +0800

    20131224

--- PyPcapAnalyzer/packet.py
+++ PyPcapAnalyzer/packet.py
@@ -4,51 +4,51 @@ import struct
 
 
 PCAP_HEADER = (
-    ('MAG', 'I', 4),    # magic number
-    ('MAJ', 'H', 2),    # majic number
-    ('MIN', 'H', 2),    # minor number
-    ('TIZ', 'I', 4),    # time zone
-    ('SIG', 'I', 4),    # timestamp
-    ('LEN', 'I', 4),    # snapshot length
-    ('LTY', 'I', 4),    # type of link-layer
+    ('MAG', 'I'),    # magic number
+    ('MAJ', 'H'),    # majic number
+    ('MIN', 'H'),    # minor number
+    ('TIZ', 'I'),    # time zone
+    ('SIG', 'I'),    # timestamp
+    ('LEN', 'I'),    # snapshot length
+    ('LTY', 'I'),    # type of link-layer
 )
 
 PACKET_HEADER = (
-    ('TSS', 'I', 4),    # timestamp in second
-    ('TSM', 'I', 4),    # timestamp in microsecond
-    ('CLEN', 'I', 4),   # length of packet data that were caputure
-    ('LEN', 'I', 4),    # the actual length of the packet
+    ('TSS', 'I'),    # timestamp in second
+    ('TSM', 'I'),    # timestamp in microsecond
+    ('CLEN', 'I'),   # length of packet data that were caputure
+    ('LEN', 'I'),    # the actual length of the packet
 )
 
 ETHERNET_HEADER = (
-    ('DST', '6B', 6),   # destination MAC address
-    ('SRC', '6B', 6),   # source MAC address
-    ('PTO', 'H', 2),    # protocol of network-layer
+    ('DST', '6s'),   # destination MAC address
+    ('SRC', '6s'),   # source MAC addr
+    ('PTO', 'H'),    # protocol of network-layer
 )
 
 IP_HEADER = (
-    ('VL', 'B', 1),     # version and length of header
-    ('TOS', 'B', 1),    # type of services
-    ('LEN', 'H', 2),    # length of the packet
-    ('ID', 'H', 2),     # identification of the packet
-    ('OFF', 'H', 2),    # offset of the segment
-    ('TTL', 'B', 1),    # time to live
-    ('PTO', 'B', 1),    # protocol of transport layer
-    ('CKS', 'H', 2),    # checksum
-    ('SRC', 'I', 4),    # source IP address
-    ('DST', 'I', 4),    # destination IP address
+    ('VL', 'B'),     # version and length of header
+    ('TOS', 'B'),    # type of services
+    ('LEN', 'H'),    # length of the packet
+    ('ID', 'H'),     # identification of the packet
+    ('OFF', 'H'),    # offset of the segment
+    ('TTL', 'B'),    # time to live
+    ('PTO', 'B'),    # protocol of transport layer
+    ('CKS', 'H'),    # checksum
+    ('SRC', '4s'),    # source IP address
+    ('DST', '4s'),    # destination IP address
 )
 
 TCP_HEADER = (
-    ('SRC', 'H', 2),
-    ('DST', 'H', 2),
-    ('SEQ', 'I', 4),
-    ('ACK', 'I', 4),
-    ('LEN', 'B', 1),
-    ('FLG', 'B', 1),
-    ('WIN', 'H', 2),
-    ('CKS', 'H', 2),
-    ('URP', 'H', 2),
+    ('SRC', 'H'),    # source port
+    ('DST', 'H'),    # destination port
+    ('SEQ', 'I'),    # sequence number
+    ('ACK', 'I'),    # acknowledgment number
+    ('LEN', 'B'),    # data offset
+    ('FLG', 'B'),    # flags
+    ('WIN', 'H'),    # window size
+    ('CKS', 'H'),    # checksum
+    ('URP', 'H'),    # urgent pointer
 )
 
 
@@ -64,19 +64,23 @@ class Packet:
             self.ip_header['MF'] = self.ip_header['OFF'] & 0x2000
             self.ip_header['OFF'] = self.ip_header['OFF'] & 0x1FFF
 
+    def tcp_header_format(self):
+        if self.tcp_header is None:
+            raise AttributeError()
+        else:
+            self.tcp_header['LEN'] = (self.tcp_header['LEN'] >> 4) * 4
+            self.tcp_header['SYN'] = (self.tcp_header['FLG'] & (0b00000001 << 1)) >> 1
+            self.tcp_header['RST'] = (self.tcp_header['FLG'] & (0b00000001 << 2)) >> 2
+            self.tcp_header['PSH'] = (self.tcp_header['FLG'] & (0b00000001 << 3)) >> 3
+            self.tcp_header['ACK'] = (self.tcp_header['FLG'] & (0b00000001 << 4)) >> 4
+            self.tcp_header['URG'] = (self.tcp_header['FLG'] & (0b00000001 << 5)) >> 5
+            self.tcp_header.pop('FLG')
+
     def ip_address_format(ip):
-        s = []
-        while ip:
-            tmp = ip % 256
-            s.insert(0,str(tmp))
-            ip //= 256
-        return str(".".join(s))
+        return '.'.join( [ "%d" % x for x in bins ] )
 
     def mac_address_format(mac):
-        s = []
-        for i in mac:
-            s.append(hex(i)[2:])
-        return str(":".join(s))
+        return ':'.join( [ "%02X" % x for x in mac ] )
 
 
 class PacketTypeError(Exception):
@@ -94,11 +98,6 @@ def parse_header(header_data, header_structure, order='B'):
     index = 0
     header = {}
     for x in header_structure:
-        if len(x[1]) == 1 or x[1][1] == 's':
-            header[x[0]] = lst[index]
-            index += 1
-        else:
-            ls_len = int(x[1][0])
-            header[x[0]] = lst[index:index + ls_len]
-            index += ls_len
+        header[x[0]] = lst[index]
+        index += 1
     return header
--- PyPcapAnalyzer/pcap.py
+++ PyPcapAnalyzer/pcap.py
@@ -19,8 +19,11 @@ def read_pcap(file_name):
                 packet.ethernet_header = parse_header(file.read(14), ETHERNET_HEADER)
                 if packet.ethernet_header['PTO'] == 0x0800:
                     packet.ip_header = parse_header(file.read(20), IP_HEADER)
+                    packet.ip_header_format()
+                    file.seek(packet.ip_header['HLEN'] - 20, 1)
                     if packet.ip_header['PTO'] == 6:
                         packet.tcp_header = parse_header(file.read(20), TCP_HEADER)
+                        packet.tcp_header_format()
                     else if packet.ip_header['PTO'] == 17:
                         packet.udp_header = parse_header(file.read(), UDP_HEADER)
                     else:
